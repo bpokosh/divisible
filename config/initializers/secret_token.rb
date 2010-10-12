a5851e6ad1256441147c836b4f30b0a098028431
Brian Pokosh <bpokosh@gmail.com>
2010-10-12 11:18:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Divisible::Application.config.secret_token = '0c32a5e62a1374aba4f72e3d8aacc42c9e6ad7ebe79cbf95d0750e95191e5592c5504fd54bc83b75f3ebe9746238bf3a86f9b3d6ff3f97caa9d7bc878ca011ea'
